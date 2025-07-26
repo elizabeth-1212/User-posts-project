d22562210353466048a1709661f4f1ed868077b1
Elizabeth <elizabethcommon12@gmail.com>
2025-07-25 22:07:46 -0400

    posts1

---
+++ user.js
@@ -0,0 +1,18 @@
+const postListEl = document.querySelector('.post-list');
+async function main() {
+    const id = localStorage.getItem("id");
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
+    
+    postListEl.innerHTML = postsData.map(post => 
+        `<div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`
+    ).join('');
+}
+main();
